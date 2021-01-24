30f243c95819a0101ee83e84519c0d1cbe45f413
milenadob <56935119+milenadob@users.noreply.github.com>
2021-01-24 19:47:50 +0100

    progress bar updating for text chabged in line edit

--- game_window.py
+++ game_window.py
@@ -15,9 +15,8 @@ class GameWindow(QFrame):
         self.data = self.game_logic.load_data(file_name)
         self.prepared_data = []
         self.prepare_data()
-
-        self.game_widget = GameWidget(self.prepared_data)
-        self.game_menu = GameWindowMenu()
+        self.game_menu = GameWindowMenu(self.data)
+        self.game_widget = GameWidget(self.prepared_data, self.game_menu)
 
         window_layout = QGridLayout()
         window_layout.addWidget(self.game_widget, 0, 0)
@@ -28,36 +27,66 @@ class GameWindow(QFrame):
 
     def prepare_data(self):
         if len(self.data) % 3 == 2:
-            self.data.append(" ")
+            self.data.append("")
         elif len(self.data) % 3 == 1:
-            self.data.append(" ")
-            self.data.append(" ")
+            self.data.append("")
+            self.data.append("")
         self.prepared_data = ([(self.data[i], self.data[i+1], self.data[i+2]) for i in range(0, len(self.data), 3)])
 
 
 class GameWindowMenu(QFrame):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, data, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.progress_bar = QProgressBar(self)
         self.progress_bar.setGeometry(10, 10, 100, 30)
         self.time_label = QLabel()
 
+        self.data = data
+        self.progress_total = 0
+        self.progress_step = 1
+        self.characters_to_progress = 1
+        self.progress = 0
+
         layout = QVBoxLayout()
         layout.addWidget(self.progress_bar)
         self.setLayout(layout)
 
+        self.calculate_progress_step()
+
+    def calculate_progress_step(self):
+        self.progress_total = sum(len(i) for i in self.data)
+        characters_per_step = round(self.progress_total / 100)
+        progress_per_character = round(100 / self.progress_total)
+        if characters_per_step > progress_per_character:
+            self.characters_to_progress = characters_per_step
+        else:
+            self.progress_step = progress_per_character
+
+    def set_progress_bar(self, forward):
+        if forward:
+            self.progress_bar.setValue(self.progress + self.progress_step)
+            self.progress += self.progress_step
+        else:
+            self.progress_bar.setValue(self.progress - self.progress_step)
+            self.progress -= self.progress_step
+
+
+
 
 class GameWidget(QFrame):
-    def __init__(self, data, *args, **kwargs):
+    def __init__(self, data, menu, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.data = data
+        self.game_menu = menu
+        self.data1, self.data2, self.data3 = zip(*self.data)
 
         self.show_text_label = QLabel()
         self.show_text_label2 = QLabel()
         self.show_text_label3 = QLabel()
-        self.data = data
-        self.data1, self.data2, self.data3 = zip(*self.data)
+
         self.text_input = CustomLineEdit()
         self.text_input.returnPressed.connect(self.change_text_input2)
         self.text_input.textChanged.connect(self.check_text_input)
@@ -88,11 +117,18 @@ class GameWidget(QFrame):
         self.setFixedWidth(1100)
 
         self.line_index = 0
-        self.progress = 0
-        self.lines = len(self.data)
+        self.good_characters = 0
+        self.bad_characters = 0
+        self.deleted_characters = 0
+        self.inputs_text_len = 0
+        self.all_inputs_text_len = 0
+        self.previous_progress = 0
+
         self.update_label()
 
     def change_text_input(self):
+        self.inputs_text_len = 0
+        self.previous_progress = self.game_menu.progress
 
         if (len(self.text_input.text()) == len(self.data1[self.line_index]) and
                 self.text_input.text() in self.data1[self.line_index] and
@@ -107,11 +143,17 @@ class GameWidget(QFrame):
             self.text_input.clear()
             self.text_input2.clear()
             self.text_input3.clear()
+        if self.data1[self.line_index] == "":
+            self.game_menu.progress_bar.setValue(100)
 
     def change_text_input2(self):
+        self.inputs_text_len = 0
+        self.previous_progress = self.game_menu.progress
         self.text_input2.setFocus()
 
     def change_text_input3(self):
+        self.inputs_text_len = 0
+        self.previous_progress = self.game_menu.progress
         self.text_input3.setFocus()
 
     def update_label(self):
@@ -125,14 +167,8 @@ class GameWidget(QFrame):
         self.show_text_label3.setText(value3)
 
         # if self.line_index + 1 == self.lines:
-        #     #self.game_menu.progress_bar.setValue(100)
         #     #self.game_logic.end_game()
         #     self.time_label.setText(str(self.game_logic.total_time))
-        # elif self.line_index == 0:
-        #     self.game_menu.progress_bar.setValue(0)
-        # else:
-        #     self.progress += int(100 / self.lines)
-        #     self.game_menu.progress_bar.setValue(self.progress)
 
     def check_text_input(self, text):
         self.text_input.setStyleSheet('color:rgba(112,112,165,180);')
@@ -143,12 +179,47 @@ class GameWidget(QFrame):
             if self.sender() is self.text_input:
                 if text not in self.data1[self.line_index]:
                     self.text_input.setStyleSheet('color:red')
+                    self.bad_characters += 1
+                else:
+                    self.good_characters += 1
             if self.sender() is self.text_input2:
                 if text not in self.data2[self.line_index]:
                     self.text_input2.setStyleSheet('color:red')
+                    self.bad_characters += 1
+                else:
+                    self.good_characters += 1
             if self.sender() is self.text_input3:
                 if text not in self.data3[self.line_index]:
                     self.text_input3.setStyleSheet('color:red')
+                    self.bad_characters += 1
+                else:
+                    self.good_characters += 1
+            self.inputs_text_len += 1
+
+            if len(text) < self.inputs_text_len:
+                self.inputs_text_len -= 2
+                self.deleted_characters += 1
+                if self.bad_characters > 1:
+                    self.deleted_characters = 0
+                    self.bad_characters -= 2
+                elif self.bad_characters == 1:
+                    self.bad_characters = 0
+                    self.deleted_characters = 0
+                    self.good_characters -= 1
+                if self.deleted_characters == self.game_menu.characters_to_progress:
+                    self.game_menu.set_progress_bar(False)
+                    self.deleted_characters = 0
+                    self.good_characters = 0
+                if len(text) == 1:
+                    self.inputs_text_len = 0
+                    self.deleted_characters = 0
+                    self.good_characters = 0
+                    self.game_menu.progress_bar.setValue(self.previous_progress)
+                    self.game_menu.progress = self.previous_progress
+
+            elif self.good_characters == self.game_menu.characters_to_progress:
+                self.game_menu.set_progress_bar(True)
+                self.good_characters = 0
 
 
 class CustomLineEdit(QLineEdit):
